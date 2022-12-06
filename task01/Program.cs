30c6c5e0c33fe372e534938bd70fcd723649019d
Andrey Ovchinnikov <sunnyhand@bk.ru>
2022-12-06 20:24:25 +0300

    Первая задача семинара

---
+++ task01/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Введите координату Х: ");
+int X = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y: ");
+int X = int.Parse(Console.ReadLine());
+
+if (X>0 $$ Y>0)
+{
+    Console.WriteLine("I");
+}
+if (X<0 $$ Y>0)
+{
+    Console.WriteLine("II");
+}
+if (X<0 $$ Y<0)
+{
+    Console.WriteLine("III");
+}
+if (X>0 $$ Y<0)
+{
+    Console.WriteLine("IV");
+}
